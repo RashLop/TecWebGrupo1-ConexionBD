8c5acb5c36f9897b44cd6d5c0b2debacc05b9940
Soria <rene.soria@ucb.edu.bo>
2025-10-04 23:13:37 -0400

    feat: add IEventService

---
+++ Services/IEventService.cs
@@ -0,0 +1,13 @@
+﻿using Grupo1Tarea.Models;
+using Grupo1Tarea.Models.dtos;
+
+namespace Grupo1Tarea.Services
+{
+    public interface IEventService
+    {
+        Task<IEnumerable<Event>> GetAll();
+        Task<Event?> GetById(Guid id);
+        Task<Event> Create(CreateEventDto dto);
+        Task<bool> Delete(Guid id);
+    }
+}
