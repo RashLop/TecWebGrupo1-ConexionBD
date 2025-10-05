cdacc4ae83d89651f363469568e232aff4db9c0c
Soria <rene.soria@ucb.edu.bo>
2025-10-04 22:56:01 -0400

    feat: add IEventRepository

---
+++ Repositories/IEventRepository.cs
@@ -0,0 +1,12 @@
+﻿using Grupo1Tarea.Models;
+
+namespace Grupo1Tarea.Repositories
+{
+    public interface IEventRepository
+    {
+        Task<IEnumerable<Event>> GetAll();
+        Task<Event?> GetById(Guid id);
+        Task Add(Event evt);
+        Task Delete(Guid id);
+    }
+}
