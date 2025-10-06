f3150f242310baa841e29caac74e1ab1d99754f0
Luis Aguilar <luis.aguilar.c@ucb.edu.bo>
2025-10-05 22:51:27 -0400

    feat: create model Guest

---
+++ Models/Guest.cs
@@ -0,0 +1,9 @@
+﻿namespace Grupo1Tarea.Models
+{
+    public class Guest
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public bool Confirmed { get; set; }
+    }
+}
