26ce0ebb2e6eb887b835dd18da1ff9b2a3506996
Luis Aguilar <luis.aguilar.c@ucb.edu.bo>
2025-10-06 00:18:21 -0400

    feat: create GuestService and IGuestService

---
+++ Services/GuestService.cs
@@ -0,0 +1,49 @@
+﻿using Grupo1Tarea.Models;
+using Grupo1Tarea.Models.dtos;
+using Grupo1Tarea.Repositories;
+
+namespace Grupo1Tarea.Services
+{
+    public class GuestService : IGuestService
+    {
+        private readonly IGuestRepository _repo;
+        public GuestService(IGuestRepository repo)
+        {
+            _repo = repo;
+        }
+        public async Task<Guest> Create(CreateGuestDto dto)
+        {
+            if (string.IsNullOrWhiteSpace(dto.FullName))
+            {
+                throw new InvalidOperationException("Full name required");
+            }
+            var guest = new Guest
+            {
+                Id = Guid.NewGuid(),
+                FullName = dto.FullName.Trim(),
+                Confirmed = dto.Confirmed,
+            };
+            await _repo.Add(guest);
+            return guest;
+        }
+
+        public async Task<bool> Delete(Guid id)
+        {
+            var existing = await _repo.GetById(id);
+            if (existing == null) return false;
+            await _repo.Delete(id);
+            return true;
+        }
+
+        public async Task<IEnumerable<Guest>> GetAll()
+        {
+            return await _repo.GetAll();
+        }
+
+        public async Task<Guest?> GetById(Guid id)
+        {
+            return await _repo.GetById(id);
+        }
+    }
+}
+
---
+++ Services/IGuestService.cs
@@ -0,0 +1,15 @@
+﻿using Grupo1Tarea.Models;
+using Grupo1Tarea.Models.dtos;
+
+namespace Grupo1Tarea.Services
+{
+    public interface IGuestService
+    {
+        Task<IEnumerable<Guest>> GetAll();
+        Task<Guest?> GetById(Guid id);
+        Task<Guest> Create(CreateGuestDto dto);
+
+
+        Task<bool> Delete(Guid id);
+    }
+}
